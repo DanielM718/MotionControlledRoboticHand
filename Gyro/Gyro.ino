55a31408eb96104f579afc0a10db719251177cbf
DanielM718 <danielmejia0405@gmail.com>
2024-01-30 17:05:37 -0500

    mpu6050 program file

---
+++ Gyro/Gyro.ino
@@ -0,0 +1,278 @@
+//#include <MPU6050.h>
+
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
+
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+// these are the adafruit graphics and OLED library so we can actually display something on the screen
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// reset pin is not used in my OLED model but it is required to use the library defined above
+#define OLED_RESET 14
+// this will represent the OLED in the program
+Adafruit_SSD1306 display(OLED_RESET);
+// This is the I2C address for the OLED display
+#define SCREEN_ADDRESS 0x3C
+
+// class default I2C address is 0x68
+MPU6050 mpu(0x68);
+
+#define OUTPUT_READABLE_QUATERNION
+
+#define OUTPUT_READABLE_EULER
+
+#define OUTPUT_READABLE_YAWPITCHROLL
+
+#define OUTPUT_READABLE_REALACCEL
+
+
+#define INTERRUPT_PIN 19  // use pin 2 on Arduino Uno & most boards
+
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+
+
+// ================================================================
+// ===                      INITIAL SETUP                       ===
+// ================================================================
+
+void setup() {
+  // initialize serial communication
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.display();
+   // Pause for 2 seconds
+  // Clear the buffer
+  display.clearDisplay();
+  delay(5000);
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+      Wire.begin();
+      Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+      Fastwire::setup(400, true);
+  #endif
+  pinMode(INTERRUPT_PIN, INPUT);
+  delay(2000); // Pause for 2 seconds
+
+
+  // initialize device
+  Serial.println(F("Initializing I2C devices..."));
+  delay(50);
+  Serial.println("Before mpu.initialize()");
+  mpu.initialize();
+  Serial.println("After mpu.initialize()");
+  delay(50);
+
+  delay(2000); // Pause for 2 seconds
+
+  
+  // verify connection
+  Serial.println(F("Testing device connections..."));
+  Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+  // wait for ready
+  //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+  //while (Serial.available() && Serial.read()); // empty buffer
+  //while (!Serial.available());                 // wait for data
+  //while (Serial.available() && Serial.read()); // empty buffer again
+  delay(2000);
+  // load and configure the DMP
+  Serial.println(F("Initializing DMP..."));
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  // make sure it worked (returns 0 if so)
+  if (devStatus == 0) {
+      // Calibration Time: generate offsets and calibrate our MPU6050
+      mpu.CalibrateAccel(6);
+      mpu.CalibrateGyro(6);
+      mpu.PrintActiveOffsets();
+      // turn on the DMP, now that it's ready
+      Serial.println(F("Enabling DMP..."));
+      mpu.setDMPEnabled(true);
+
+      // enable Arduino interrupt detection
+      Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+      Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+      Serial.println(F(")..."));
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      Serial.println(F("DMP ready! Waiting for first interrupt..."));
+      dmpReady = true;
+
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
+  } else {
+      // ERROR!
+      // 1 = initial memory load failed
+      // 2 = DMP configuration updates failed
+      // (if it's going to break, usually the code will be 1)
+      Serial.print(F("DMP Initialization failed (code "));
+      Serial.print(devStatus);
+      Serial.println(F(")"));
+  }
+
+  // configure LED for output
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
+void loop() {
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+  // read a packet from FIFO
+  if (mpu.dmpGetCurrentFIFOPacket(fifoBuffer)) { // Get the Latest packet 
+    #ifdef OUTPUT_READABLE_QUATERNION
+      // display quaternion values in easy matrix form: w x y z
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      Serial.print("quat\t");
+      Serial.print(q.w);
+      Serial.print("\t");
+      Serial.print(q.x);
+      Serial.print("\t");
+      Serial.print(q.y);
+      Serial.print("\t");
+      Serial.println(q.z);
+
+      display.clearDisplay();
+      display.setTextSize(1);
+      display.setTextColor(SSD1306_WHITE);
+      display.setCursor(0,0);
+      display.print(F("quat\t"));
+      display.print(q.w);
+      display.print("\t");
+      display.print(q.x);
+      display.print("\t");
+      display.println(q.y);
+      display.println(q.z);
+      display.display();
+    #endif
+
+    #ifdef OUTPUT_READABLE_EULER
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetEuler(euler, &q);
+      Serial.print("euler\t");
+      Serial.print(euler[0] * 180/M_PI);
+      Serial.print("\t");
+      Serial.print(euler[1] * 180/M_PI);
+      Serial.print("\t");
+      Serial.println(euler[2] * 180/M_PI);
+
+      // display.clearDisplay();
+      // display.setTextSize(1);
+      // display.setTextColor(SSD1306_WHITE);
+      // display.setCursor(0,0);
+      // display.print(F("ypr\t"));
+      // display.print(euler[0] * 180/M_PI);
+      // display.print("\t");
+      // display.print(euler[1] * 180/M_PI);
+      // display.print("\t");
+      // display.println(euler[2] * 180/M_PI);
+      // display.display();
+    #endif
+
+    #ifdef OUTPUT_READABLE_YAWPITCHROLL
+      // display Euler angles in degrees
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+      Serial.print("ypr\t");
+      Serial.print(ypr[0] * 180/M_PI);
+      Serial.print("\t");
+      Serial.print(ypr[1] * 180/M_PI);
+      Serial.print("\t");
+      Serial.println(ypr[2] * 180/M_PI);
+
+      // display.setTextSize(1);
+      // display.setTextColor(SSD1306_WHITE);
+      // display.setCursor(0,10);
+      // display.print(F("ypr\t"));
+      // display.print(ypr[0] * 180/M_PI);
+      // display.print("\t");
+      // display.print(ypr[1] * 180/M_PI);
+      // display.print("\t");
+      // display.println(ypr[2] * 180/M_PI);
+      // display.display();
+    #endif
+
+    #ifdef OUTPUT_READABLE_REALACCEL
+      // display real acceleration, adjusted to remove gravity
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetAccel(&aa, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+      Serial.print("areal\t");
+      Serial.print(aaReal.x);
+      Serial.print("\t");
+      Serial.print(aaReal.y);
+      Serial.print("\t");
+      Serial.println(aaReal.z);
+
+      // display.clearDisplay();
+      // display.setTextSize(1);
+      // display.setTextColor(SSD1306_WHITE);
+      // display.setCursor(0,0);
+      // display.print(F("areal\t"));
+      // display.print(aaReal.x);
+      // display.print("\n");
+      // display.print(aaReal.y);
+      // display.print("\n");
+      // display.print(aaReal.z);
+      // display.display();
+    #endif
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LED_PIN, blinkState);
+  }
+
+}
+
