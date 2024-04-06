52620d35f99ba28dd45bfb3b84f306f333b0b2ef
DanielM718 <danielmejia0405@gmail.com>
2024-04-05 22:40:46 -0400

    BLE Connector

--- BLE/BLEReceiver.py
+++ BLE/BLEReceiver.py
@@ -1,10 +1,14 @@
 import asyncio
 from bleak import BleakClient
 
-async def main():
-    async with BleakClient("D8:BC:38:E5:2A:66") as client:
-    	print(client.services)
+
+async def main(address):
+    async with BleakClient(address) as client:
+    	for service in client.services:
+    		print(service)
+    		for char in service.chracteristics:
+    			print(char)
     	
     	
     	
-asyncio.run(main())
+asyncio.run(main("D8:BC:38:E5:2A:66"))
